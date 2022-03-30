93e6cd7ff4f7f5c1050a09312a15762f8f71bb52
Calvin Molitor <52789184+caljay98@users.noreply.github.com>
2022-03-30 15:59:58 -0500

    starting to debug the converter

--- DLM_data_parser/gdat_to_Id/gdat_to_Id.c
+++ DLM_data_parser/gdat_to_Id/gdat_to_Id.c
@@ -15,8 +15,8 @@
 START_OF_FILE_t sof_data = {0};
 FILE_METADATA_t file_data = {0};
 
-const char edl_ver_string[] = EDL_VERSION_STRING;
-const char magic_string[] = MAGIC_STRING;
+const U8 edl_ver_string[] = EDL_VERSION_STRING;
+const U8 magic_string[] = MAGIC_STRING;
 
 
 // the head node WILL NOT be written in memory so it do not count it
@@ -48,12 +48,15 @@ int main(int argc, char** argv)
     // TODO
 
     // DEBUG filling in the sof and metadata
+    init_sof_block(&sof_data, 2022, 3, 30, 4, 20, 55, "SESSION", "SHORT COMMENT", "TEAM NAME");
+    init_metadata_block(&file_data, "EVENT NAME", "SESSION", "LONG COMMENT", "LOCATION");
 
     // DEBUG add a couple of data points for testing
     // Doing 100 data points at 10 Hz
     U32 buf[100];
     for (int c = 0; c < 100; c++) buf[c] = c;
     add_channel_to_list(&channel_head, 100, buf, 10, 0, 1, 1, 0, "tester variable", "test", "unit");
+    add_channel_to_list(&channel_head, 100, buf, 10, 0, 1, 1, 0, "tester variable 2", "test2", "unit2");
 
     // Run the "linker". This will fill out correct file space for all of the different
     // blocks of data and makes sure they will be correctly pointed to in the file
@@ -61,7 +64,9 @@ int main(int argc, char** argv)
 
     // Open a new file with the correct name and begin writing all of the data to it.
     // Layout the file as planned after the linker was run
-    // TODO
+    write_id_file(&sof_data, &file_data, &channel_head, "test_Id.Id");
+
+    printf("Conversion sucessful\n");
 }
 
 
@@ -77,9 +82,14 @@ S8 add_channel_to_list(CHANNEL_DESC_LL_NODE_t* chan_head, U32 num_data_points, U
     // make a new LL node and put it in the list, updating the prev and next
     // pointers accordingly
     new_chan = (CHANNEL_DESC_LL_NODE_t*)malloc(sizeof(CHANNEL_DESC_LL_NODE_t));
-    if (!new_chan) return -1;
+    if (!new_chan)
+    {
+        printf("failed a malloc\n");
+        return -1;
+    }
     new_chan->next = chan_head->next;
     new_chan->prev = chan_head;
+    chan_head->next = new_chan;
     if (new_chan->next) new_chan->next->prev = new_chan;
 
     // assign the things that can be just copied
@@ -113,8 +123,8 @@ S8 init_sof_block(START_OF_FILE_t* sof, U16 year, U8 month, U8 day, U8 hour, U8 
     // fill in the things that dont change
     sof->version_string_fptr = EDL_VER_STR_FLOC;
     sof->dash_version_string_fptr = 0x0;
-    strncpy(sof->edl_version_string, edl_ver_string, sizeof(sof->edl_version_string));
-    strncpy(sof->magic_str, magic_string, sizeof(sof->magic_str));
+    memcpy(sof->edl_version_string, edl_ver_string, sizeof(sof->edl_version_string));
+    memcpy(sof->magic_str, magic_string, sizeof(sof->magic_str));
 
     // fill in the time and data with sprintfs. Make sure to keep the lengths correct
     sprintf(time_str, "%02d:%02d:%02d", hour, minute, second);
@@ -124,8 +134,8 @@ S8 init_sof_block(START_OF_FILE_t* sof, U16 year, U8 month, U8 day, U8 hour, U8 
 
     // fill in the strings
     strncpy(sof->session_str, session, sizeof(sof->session_str));
-    strncpy(sof->short_comment_str, session, sizeof(sof->short_comment_str));
-    strncpy(sof->team_name_str, session, sizeof(sof->team_name_str));
+    strncpy(sof->short_comment_str, short_comment, sizeof(sof->short_comment_str));
+    strncpy(sof->team_name_str, team_name, sizeof(sof->team_name_str));
 
     return 0;
 }
@@ -160,7 +170,7 @@ S8 link_id_file(START_OF_FILE_t* sof, FILE_METADATA_t* metadat, CHANNEL_DESC_LL_
     while (chan_ptr != NULL)
     {
         num_channels++;
-        chan_ptr++;
+        chan_ptr = chan_ptr->next;
     }
     sof->data_start_fptr = CHANNEL_DESC_START_FLOC + (num_channels * CHANNEL_DESC_SIZE);
 
@@ -170,20 +180,144 @@ S8 link_id_file(START_OF_FILE_t* sof, FILE_METADATA_t* metadat, CHANNEL_DESC_LL_
 
     // for each of the channels, fill in the pointers to previous and next
     chan_ptr = chan_head->next;
+    curr_file_loc = CHANNEL_DESC_START_FLOC;
     while (chan_ptr != NULL)
     {
-        // TODO
+        // set this nodes location and 
+        chan_ptr->location_fptr = curr_file_loc;
+        curr_file_loc += CHANNEL_DESC_SIZE;
+
+        if (chan_ptr->prev == chan_head)
+        {
+            chan_ptr->channel_desc.prev_fptr = 0;
+        }
+        else
+        {
+            // if the node before is not the head, set the location of this node to it and
+            // set the location of the previous node to this onme
+            chan_ptr->channel_desc.prev_fptr = chan_ptr->prev->location_fptr;
+            chan_ptr->prev->channel_desc.next_fptr = chan_ptr->location_fptr;
+        }
+
+        // set the file location of the next node to 0, it will get fixed if the next node
+        // is not null next run through the loop
+        chan_ptr->channel_desc.next_fptr = 0;
+
+        chan_ptr = chan_ptr->next;
     }
 
-    // at the end of the channels, start putting the data buffers and add the location to the channel
-    // TODO
+    // at the end of the channels, start putting the data buffers and add the location to the
+    // channel. The curr_file_loc is now at the start of the data
+    chan_ptr = chan_head->next;
+    while (chan_ptr != NULL)
+    {
+        chan_ptr->channel_desc.data_buf_fptr = curr_file_loc;
+
+        // move the curr_file_loc to be right after this block of data
+        if (chan_ptr->channel_desc.data_size == s16_data)
+        {
+            // 16bit data points
+            curr_file_loc += (chan_ptr->channel_desc.num_data_points * sizeof(U16));
+        }
+        else
+        {
+            // 32bit data points
+            curr_file_loc += (chan_ptr->channel_desc.num_data_points * sizeof(U32));
+        }
+
+        chan_ptr = chan_ptr->next;
+    }
 }
 
 
 // write_id_file
 //  take the parts of the files and write them. Be sure to look at the fptrs in the data
-//  to put everything where it belongs in the file
-S8 write_id_file(START_OF_FILE_t* sof, FILE_METADATA_t* metadat, CHANNEL_DESC_LL_NODE_t* chan_head)
+//  to put everything where it belongs in the file. All values must be little endian
+S8 write_id_file(START_OF_FILE_t* sof, FILE_METADATA_t* metadat, CHANNEL_DESC_LL_NODE_t* chan_head,
+                 const char* filename)
+{
+    FILE* file;
+    CHANNEL_DESC_LL_NODE_t* chan_ptr;
+    U8 sof_str[START_OF_FILE_SIZE] = {0};
+    U8 metadat_str[FILE_METADATA_SIZE] = {0};
+    U8 chan_desc_str[CHANNEL_DESC_SIZE] = {0};
+
+    // make the new file
+    file = fopen(filename, "w");
+    if (!file)
+    {
+        printf("failed to open file\n");
+        return -1;
+    }
+
+    // if needed, convert all of the data to little endian
+    // Linux with AMD: Already big endian
+    // WSL and intel: TODO
+
+    // fill in the byte strings for the sof and metadata
+    // TODO
+
+    // write the SOF block, this will be at the start of the file
+    fseek(file, 0, SEEK_SET);
+    fwrite(sof_str, sizeof(sof_str), 1, file);
+
+    // move to the location of the file metadata and write it
+    fseek(file, FILE_METADATA_FLOC, SEEK_SET);
+    fwrite(metadat_str, sizeof(metadat_str), 1, file);
+
+    // for each channel, write it. Start in the correct location
+    fseek(file, CHANNEL_DESC_START_FLOC, SEEK_SET);
+    chan_ptr = chan_head->next;
+
+    while(chan_ptr != NULL)
+    {
+        // convert to a byte string and write
+        // TODO
+
+        fwrite(chan_desc_str, sizeof(chan_desc_str), 1, file);
+        chan_ptr = chan_ptr->next;
+    }
+
+    // for each channel, write the data buffer. This is just continuous memory
+    chan_ptr = chan_head->next;
+    while(chan_ptr != NULL)
+    {
+        if (chan_ptr->channel_desc.data_size == s16_data)
+        {
+            // 16bit data points
+            fwrite(chan_ptr->data_buffer, chan_ptr->channel_desc.num_data_points * sizeof(U16),
+                   1, file);
+        }
+        else
+        {
+            // 32bit data points
+            fwrite(chan_ptr->data_buffer, chan_ptr->channel_desc.num_data_points * sizeof(U32),
+                   1, file);
+        }
+        chan_ptr = chan_ptr->next;
+    }
+}
+
+
+// sof_to_bytes
+//  convert the SOF struct into a raw byte string for writing
+void sof_to_bytes(START_OF_FILE_t* sof, U8* bytes)
+{
+    // TODO
+}
+
+
+// metadata_to_bytes
+//  convert the metadata struct into a raw byte string for writing
+void metadata_to_bytes(FILE_METADATA_t* metadat, U8* bytes)
+{
+    // TODO
+}
+
+
+// channel_desc_to_bytes
+//  convert the channel description into a raw byte string for writing
+void channel_desc_to_bytes(CHANNEL_DESC_t* chan_desc, U8* bytes)
 {
     // TODO
 }
